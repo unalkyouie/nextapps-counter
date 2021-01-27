c277e3808abb7fba0ad6e74075e1348a20028273
unknown <alk.pikus@gmail.com>
2021-01-27 14:29:37 +0100

    refactor: added screen to App

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 
+import Main from './screens/Main';
+
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <Main />
+    </SafeAreaView>
+  );
 };
 export default App;
