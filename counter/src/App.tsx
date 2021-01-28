396bbece1dce86f1121a53b2b24ae3b379bcc8ee
unknown <alk.pikus@gmail.com>
2021-01-28 13:48:03 +0100

    refactor: added styles to Main screen and App

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Main from './screens/Main';
@@ -8,10 +8,17 @@ import store from './store';
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Main />
       </SafeAreaView>
     </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E5B3BB',
+    flex: 1,
+  },
+});
 export default App;
--- counter/src/screens/Main.tsx
+++ counter/src/screens/Main.tsx
@@ -7,7 +7,7 @@ import { AppState } from '../reducers';
 import { decrement, increment } from '../reducers/counterReducer';
 import { AppDispatch } from '../store';
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 const Main = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -17,46 +17,62 @@ const Main = () => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>COUNTER</Text>
+      </View>
       <View style={styles.counterWrapper}>
-        <Text style={styles.counterText}>COUNTER</Text>
-        <Text style={styles.counterText}>{counterValue}</Text>
-        <View style={styles.buttonsWrapper}>
-          <Button
-            type="decrement"
-            disabled={counterValue <= 0 ? true : false}
-            onPress={() => dispatch(decrement())}
-          />
-          <Button
-            type="increment"
-            disabled={counterValue >= 100 ? true : false}
-            onPress={() => dispatch(increment())}
-          />
+        <Button
+          type="decrement"
+          disabled={counterValue <= 0 ? true : false}
+          onPress={() => dispatch(decrement())}
+        />
+        <View style={styles.counterTextWrapper}>
+          <Text style={styles.counterText}>{counterValue}</Text>
         </View>
+        <Button
+          type="increment"
+          disabled={counterValue >= 100 ? true : false}
+          onPress={() => dispatch(increment())}
+        />
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    margin: 20,
-    padding: 20,
-  },
-  buttonsWrapper: {
-    flexDirection: 'row',
-  },
   container: {
     alignItems: 'center',
-    backgroundColor: 'cyan',
-    height,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
   counterWrapper: {
-    padding: 50,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 0.1 * width,
+    paddingVertical: 0.2 * height,
   },
   counterText: {
+    color: '#C54B6C',
     fontSize: 72,
+    fontWeight: 'bold',
     textAlign: 'center',
+  },
+  counterTextWrapper: {
+    width: 0.6 * width,
+  },
+  title: {
+    color: '#C54B6C',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  titleWrapper: {
+    alignItems: 'center',
+    backgroundColor: '#C6C9D0',
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    justifyContent: 'flex-end',
+    padding: 0.1 * width,
+    width,
   },
 });
 
