5ad517c59aab1881f22bfeb99eb4a4f60cd24f7b
unknown <alk.pikus@gmail.com>
2021-01-28 12:07:57 +0100

    refactor: moved dispatch to Main screen

--- counter/src/screens/Main.tsx
+++ counter/src/screens/Main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '../components/Button';
 import { AppState } from '../reducers';
+import { decrement, increment } from '../reducers/counterReducer';
+import { AppDispatch } from '../store';
 
 const { height } = Dimensions.get('window');
 
 const Main = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const counterValue = useSelector<AppState, number>(
     (state) => state.counter.counter,
   );
@@ -15,15 +18,18 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <View style={styles.counterWrapper}>
+        <Text style={styles.counterText}>COUNTER</Text>
         <Text style={styles.counterText}>{counterValue}</Text>
         <View style={styles.buttonsWrapper}>
           <Button
             type="decrement"
             disabled={counterValue <= 0 ? true : false}
+            onPress={() => dispatch(decrement())}
           />
           <Button
             type="increment"
             disabled={counterValue >= 100 ? true : false}
+            onPress={() => dispatch(increment())}
           />
         </View>
       </View>
